f4b45820de198eb567c63baf20b61b4086ba065e
flebars <frederclebars@lttd-consulting.com>
2024-06-20 16:19:32 +0200

    controle source EXT020

--- src/main/groovy/EXT020.groovy
+++ src/main/groovy/EXT020.groovy
@@ -2,173 +2,155 @@ import java.time.LocalDateTime
 import java.time.format.DateTimeFormatter
 
 /**
- * README
- * This extension is used by Mashup
- * <p>
  * Name : EXT020
+ * COMX01 Gestion des assortiments clients
  * Description : Select the items based on the criteria contained in EXT021 table and add records to the EXT020 table (EXT020MI.SelAssortItems conversion)
  * Date         Changed By   Description
  * 20240118     YYOU         COMX01 - Submit calc EXT022
  */
 
 public class EXT020 extends ExtendM3Batch {
+  private final DatabaseAPI database
+  private final LoggerAPI logger
+  private final MICallerAPI miCaller
+  private final ProgramAPI program
+  private final UtilityAPI utility
+  private final BatchAPI batch
 
-    private final MIAPI mi
-    private final DatabaseAPI database
-    private final LoggerAPI logger
-    private final MICallerAPI miCaller
-    private final ProgramAPI program
-    private final UtilityAPI utility
-    private final BatchAPI batch
-    private final TextFilesAPI textFiles
-    private int currentCompany
-    private String currentDivision
-    private String cuno = ""
-	  private String svcuno = ""
-    private String fvdt = ""
-    private String fdat = ""
-    private String ascd = ""
-    private boolean IN60 = false
-    private String rawData
-    private int rawDataLength
-    private int beginIndex
-    private int endIndex
-    private String logFileName
-    private String jobNumber
-    private String currentDate = ""
+  private int currentCompany
+  private String currentDivision
+  private String cuno = ""
+  private String svcuno = ""
+  private String fvdt = ""
+  private String fdat = ""
+  private String ascd = ""
+  private String rawData
+  private int rawDataLength
+  private int beginIndex
+  private int endIndex
+  private String jobNumber
 
-    public EXT020(LoggerAPI logger, DatabaseAPI database, UtilityAPI utility, ProgramAPI program, BatchAPI batch, MICallerAPI miCaller, TextFilesAPI textFiles) {
-        this.logger = logger
-        this.database = database
-        this.program = program
-        this.batch = batch
-        this.miCaller = miCaller
-        this.textFiles = textFiles
-        this.utility = utility
+  public EXT020(LoggerAPI logger, DatabaseAPI database, UtilityAPI utility, ProgramAPI program, BatchAPI batch, MICallerAPI miCaller, TextFilesAPI textFiles) {
+    this.logger = logger
+    this.database = database
+    this.program = program
+    this.batch = batch
+    this.miCaller = miCaller
+    this.utility = utility
+  }
+
+  public void main() {
+    // Get job number
+    LocalDateTime timeOfCreation = LocalDateTime.now()
+    jobNumber = program.getJobNumber() + timeOfCreation.format(DateTimeFormatter.ofPattern("yyMMdd")) + timeOfCreation.format(DateTimeFormatter.ofPattern("HHmmss"))
+
+    if (batch.getReferenceId().isPresent()) {
+      Optional<String> data = getJobData(batch.getReferenceId().get())
+      if (!data.isEmpty())
+        performActualJob(data)
+    } else {
+      // No job data found
     }
+  }
 
-    public void main() {
-        // Get job number
-        LocalDateTime timeOfCreation = LocalDateTime.now()
-        jobNumber = program.getJobNumber() + timeOfCreation.format(DateTimeFormatter.ofPattern("yyMMdd")) + timeOfCreation.format(DateTimeFormatter.ofPattern("HHmmss"))
-
-        //logger.debug("Début EXT020")
-        if (batch.getReferenceId().isPresent()) {
-            Optional<String> data = getJobData(batch.getReferenceId().get())
-            performActualJob(data)
-        } else {
-            // No job data found
-            //logger.debug("Job data for job ${batch.getJobId()} is missing")
-        }
-    }
-
-    // Get job data
-    private Optional<String> getJobData(String referenceId) {
-        def query = database.table("EXTJOB").index("00").selection("EXDATA").build()
-        def container = query.createContainer()
-        container.set("EXRFID", referenceId)
-        if (query.read(container)) {
-            //logger.debug("EXDATA = " + container.getString("EXDATA"))
-            return Optional.of(container.getString("EXDATA"))
-        } else {
-            //logger.debug("EXTJOB not found")
-        }
-        return Optional.empty()
-    }
-
-    // Perform actual job
-    private performActualJob(Optional<String> data) {
-        if (!data.isPresent()) {
-            //logger.debug("Job reference Id ${batch.getReferenceId().get()} is passed but data was not found")
-            return
-        }
-        rawData = data.get()
-        logger.debug("Début performActualJob")
-        String inDATE = getFirstParameter()
-
-        logger.debug("inDATE = " + inDATE)
-        
-        svcuno = ""
-        
-        currentCompany = (Integer) program.getLDAZD().CONO
-        currentDivision = program.getLDAZD().DIVI
-        LocalDateTime timeOfCreation = LocalDateTime.now()
-
-        if (inDATE != null && !inDATE.trim().isBlank()) {
-            fdat = inDATE
-            if (!utility.call("DateUtil", "isDateValid", fdat, "yyyyMMdd")) {
-                String header = "MSG;" + "FDAT"
-                String message = "Date de début est incorrecte " + ";" + fdat
-                logMessage(header, message)
-                return
-            }
-        } else {
-            fdat = timeOfCreation.format(DateTimeFormatter.ofPattern("yyyyMMdd"))
-        }
-        //Lecture des EXT010 modifié
-        ExpressionFactory expression_EXT010 = database.getExpressionFactory("EXT010")
-        expression_EXT010 = expression_EXT010.ge("EXLMDT", fdat)
-        DBAction EXT010_query = database.table("EXT010").index("02").matching(expression_EXT010).selection("EXCUNO").build()
-        DBContainer EXT010 = EXT010_query.getContainer()
-        EXT010.set("EXCONO", currentCompany)
-        if (!EXT010_query.readAll(EXT010, 1, outData_EXT010)) {
-        }
-    }
-
-    // Retrieve EXT010
-    Closure<?> outData_EXT010 = {DBContainer EXT010 ->
-        cuno = EXT010.get("EXCUNO")
-        logger.debug("EXCUNO traité" + cuno)
-        if(cuno != svcuno) {
-          DBAction OASCUS_query = database.table("OASCUS").index("00").selection("OCCONO", "OCASCD", "OCCUNO", "OCFDAT").build()
-          DBContainer OASCUS = OASCUS_query.getContainer()
-          OASCUS.set("OCCONO", currentCompany)
-          ascd = EXT010.get("EXCUNO") as String 
-          logger.debug("OCASCD à selectionner" + ascd.trim() +"0")
-          OASCUS.set("OCASCD", ascd.trim() + "0")
-          OASCUS.set("OCCUNO", cuno)
-          if (!OASCUS_query.readAll(OASCUS, 3, outData_OASCUS)) {
-          }
-        }
-        svcuno = cuno
-    }
-
-// Retrieve OASCUS
-Closure<?> outData_OASCUS = {DBContainer OASCUS ->
-        // Add selected items in the assortment
-        fvdt = OASCUS.get("OCFDAT") as String
-        ascd = OASCUS.get("OCASCD")
-        cuno = OASCUS.get("OCCUNO")
-        logger.debug("Lancement executeEXT820MISubmitBatch pour assortiment : " + ascd +"-"+cuno + "-"+fvdt)
-        executeEXT820MISubmitBatch(currentCompany as String, "EXT022", ascd, cuno, fvdt, "", "2", "", "", "")
-}
-
-// Execute EXT820MI.SubmitBatch
-private executeEXT820MISubmitBatch(String CONO, String JOID, String P001, String P002, String P003, String P004, String P005, String P006, String P007, String P008) {
-    def parameters = ["CONO":CONO, "JOID":JOID, "P001":P001, "P002":P002, "P003":P003, "P004":P004, "P005":P005, "P006":
-    P006, "P007":P007, "P008":P008]
-    Closure<?> handler = {Map < String, String > response ->
-    if (response.error != null) {
+  // Get job data
+  private Optional<String> getJobData(String referenceId) {
+    def extjobQuery = database.table("EXTJOB").index("00").selection("EXDATA").build()
+    def extjobRequest = extjobQuery.createContainer()
+    extjobRequest.set("EXRFID", referenceId)
+    if (extjobQuery.read(extjobRequest)) {
+      return Optional.of(extjobRequest.getString("EXDATA"))
     } else {
     }
+    return Optional.empty()
+  }
+
+  /**
+   * @param data
+   * @return
+   */
+  private performActualJob(Optional<String> data) {
+    if (!data.isPresent()) {
+      return
+    }
+    rawData = data.get()
+    String inDATE = getFirstParameter()
+
+    svcuno = ""
+
+    currentCompany = (Integer) program.getLDAZD().CONO
+    currentDivision = program.getLDAZD().DIVI
+    LocalDateTime timeOfCreation = LocalDateTime.now()
+
+    if (inDATE != null && !inDATE.trim().isBlank()) {
+      fdat = inDATE
+      if (!utility.call("DateUtil", "isDateValid", fdat, "yyyyMMdd")) {
+        return
+      }
+    } else {
+      fdat = timeOfCreation.format(DateTimeFormatter.ofPattern("yyyyMMdd"))
+    }
+
+    //Read EXT010 modified since fdat
+    ExpressionFactory ext010Expression = database.getExpressionFactory("EXT010")
+    ext010Expression = ext010Expression.ge("EXLMDT", fdat)
+    DBAction ext010Query = database.table("EXT010").index("02").matching(ext010Expression).selection("EXCUNO").build()
+    DBContainer ext010Request = ext010Query.getContainer()
+    ext010Request.set("EXCONO", currentCompany)
+    //no limitations in readall we have to treat all records update where LMDT > fdat
+    if (!ext010Query.readAll(ext010Request, 1, ext010Reader)) {
+    }
+  }
+
+  // Read EXT010
+  Closure<?> ext010Reader = { DBContainer ext010Result ->
+    cuno = ext010Result.get("EXCUNO")
+    if (cuno != svcuno) {
+      DBAction oascusQuery = database.table("OASCUS").index("00").selection("OCCONO", "OCASCD", "OCCUNO", "OCFDAT").build()
+      DBContainer oascusRequest = oascusQuery.getContainer()
+      oascusRequest.set("OCCONO", currentCompany)
+      ascd = ext010Result.get("EXCUNO") as String
+      oascusRequest.set("OCASCD", ascd.trim() + "0")
+      oascusRequest.set("OCCUNO", cuno)
+      if (!oascusQuery.readAll(oascusRequest, 3, 10000, oascusReader)) {
+      }
+    }
+    svcuno = cuno
+  }
+
+// Read OASCUS for each record we post
+  Closure<?> oascusReader = { DBContainer oascusResult ->
+    // Add selected items in the assortment
+    fvdt = oascusResult.get("OCFDAT") as String
+    ascd = oascusResult.get("OCASCD")
+    cuno = oascusResult.get("OCCUNO")
+    executeEXT820MISubmitBatch(currentCompany as String, "EXT022", ascd, cuno, fvdt, "", "2", "", "", "")
+  }
+
+// Execute EXT820MI.SubmitBatch
+  private executeEXT820MISubmitBatch(String CONO, String JOID, String P001, String P002, String P003, String P004, String P005, String P006, String P007, String P008) {
+    def parameters = ["CONO": CONO, "JOID": JOID, "P001": P001, "P002": P002, "P003": P003, "P004": P004, "P005": P005, "P006":
+      P006, "P007"          : P007, "P008": P008]
+    Closure<?> handler = { Map<String, String> response ->
+      if (response.error != null) {
+      } else {
+      }
     }
     miCaller.call("EXT820MI", "SubmitBatch", parameters, handler)
-}
+  }
 
 // Get first parameter
-private String getFirstParameter() {
-    //logger.debug("rawData = " + rawData)
+  private String getFirstParameter() {
     rawDataLength = rawData.length()
     beginIndex = 0
     endIndex = rawData.indexOf(";")
     // Get parameter
     String parameter = rawData.substring(beginIndex, endIndex)
-    //logger.debug("parameter = " + parameter)
     return parameter
-}
+  }
 
 // Get next parameter
-private String getNextParameter() {
+  private String getNextParameter() {
     beginIndex = endIndex + 1
     endIndex = rawDataLength - rawData.indexOf(";") - 1
     rawData = rawData.substring(beginIndex, rawDataLength)
@@ -177,28 +159,6 @@ private String getNextParameter() {
     endIndex = rawData.indexOf(";")
     // Get parameter
     String parameter = rawData.substring(beginIndex, endIndex)
-    //logger.debug("parameter = " + parameter)
     return parameter
-}
-
-// Log message
-void logMessage(String header, String message) {
-    textFiles.open("FileImport")
-    logFileName = "MSG_" + program.getProgramName() + "." + "batch" + "." + jobNumber + ".csv"
-    if (!textFiles.exists(logFileName)) {
-        log(header)
-        log(message)
-    }
-}
-
-// Log
-void log(String message) {
-    IN60 = true
-    ////logger.debug(message)
-    message = LocalDateTime.now().toString() + ";" + message
-    Closure<?> consumer = {PrintWriter printWriter ->
-            printWriter.println(message)
-    }
-    textFiles.write(logFileName, "UTF-8", true, consumer)
-}
+  }
 }

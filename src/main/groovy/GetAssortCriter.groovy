adb58c1d0823cfad438bfe5ebe93fa9375b4ca0e
flebars <frederclebars@lttd-consulting.com>
2024-06-21 09:13:22 +0200

    controle source EXT020MI

--- src/main/groovy/GetAssortCriter.groovy
+++ src/main/groovy/GetAssortCriter.groovy
@@ -1,121 +1,103 @@
 /**
- * README
  * This extension is used by Mashup
- *
  * Name : EXT020MI.GetAssortCriter
+ * COMX01 Gestion des assortiments clients
  * Description : The GetAssortCriter transaction get records to the EXT020 table.
  * Date         Changed By   Description
  * 20220112     YBLUTEAU     COMX01 - Add assortment
+ * 20240620     FLEBARS       COMX01 - Controle code pour validation Infor
  */
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
+import java.time.LocalDateTime
+
 public class GetAssortCriter extends ExtendM3Transaction {
-	private final MIAPI mi;
-	private final DatabaseAPI database
-	private final LoggerAPI logger
-	private final MICallerAPI miCaller;
-	private final ProgramAPI program;
-	private final UtilityAPI utility;
+  private final MIAPI mi
+  private final DatabaseAPI database
+  private final LoggerAPI logger
+  private final ProgramAPI program
+  private final UtilityAPI utility
 
-	public GetAssortCriter(MIAPI mi, DatabaseAPI database, LoggerAPI logger, ProgramAPI program,UtilityAPI utility) {
-		this.mi = mi;
-		this.database = database;
-		this.logger = logger;
-		this.program = program;
-		this.utility = utility;
-	}
+  public GetAssortCriter(MIAPI mi, DatabaseAPI database, LoggerAPI logger, ProgramAPI program, UtilityAPI utility) {
+    this.mi = mi
+    this.database = database
+    this.logger = logger
+    this.program = program
+    this.utility = utility
+  }
 
-	public void main() {
-		Integer currentCompany;
-		String cuno = "";
-		String ascd = "";
-		String fdat ="";
-		if (mi.in.get("CONO") == null) {
-			currentCompany = (Integer)program.getLDAZD().CONO;
-		} else {
-			currentCompany = mi.in.get("CONO");
-		}
+  public void main() {
+    Integer currentCompany
+    String cuno = ""
+    String ascd = ""
+    String fdat = ""
+    if (mi.in.get("CONO") == null) {
+      currentCompany = (Integer) program.getLDAZD().CONO
+    } else {
+      currentCompany = mi.in.get("CONO")
+    }
 
-		if(mi.in.get("CUNO") != null){
-			DBAction countryQuery = database.table("OCUSMA").index("00").build();
-			DBContainer OCUSMA = countryQuery.getContainer();
-			OCUSMA.set("OKCONO",currentCompany);
-			OCUSMA.set("OKCUNO",mi.in.get("CUNO"));
-			if (!countryQuery.read(OCUSMA)) {
-				mi.error("Code Client " + mi.in.get("CUNO") + " n'existe pas");
-				return;
-			}
-			cuno = mi.in.get("CUNO");
-		}else{
-			mi.error("Code Client est obligatoire");
-			return;
-		}
+    if (mi.in.get("CUNO") != null) {
+      cuno = mi.in.get("CUNO")
+    } else {
+      mi.error("Code Client est obligatoire")
+      return
+    }
 
-		if(mi.in.get("ASCD") != null){
-			DBAction countryQuery = database.table("CSYTAB").index("00").build();
-			DBContainer CSYTAB = countryQuery.getContainer();
-			CSYTAB.set("CTCONO",currentCompany);
-			CSYTAB.set("CTSTCO",  "ASCD");
-			CSYTAB.set("CTSTKY", mi.in.get("ASCD"));
-			if (!countryQuery.read(CSYTAB)) {
-				mi.error("Code Assortiment  " + mi.in.get("ASCD") + " n'existe pas");
-				return;
-			}
-			ascd = mi.in.get("ASCD");
-		}else{
-			mi.error("Code Assortiment  " + mi.in.get("ASCD") + " n'existe pas");
-			return;
-		}
+    if (mi.in.get("ASCD") != null) {
+      ascd = mi.in.get("ASCD")
+    } else {
+      mi.error("Code Assortiment  " + mi.in.get("ASCD") + " n'existe pas")
+      return
+    }
 
-		if(mi.in.get("FDAT") != null){
-			fdat = mi.in.get("FDAT");
-			if (!utility.call("DateUtil", "isDateValid", fdat, "yyyyMMdd")) {
-				mi.error("Format Date de Validité incorrect");
-				return;
-			}
-		}else{
-			mi.error("Date de Validité est obligatoire");
-			return;
-		}
-		LocalDateTime timeOfCreation = LocalDateTime.now();
-		DBAction query = database.table("EXT020").index("00").selection("EXCONO", "EXASCD", "EXCUNO", "EXFDAT", "EXSTAT", "EXSTTS", "EXNDTS", "EXRGDT", "EXRGTM", "EXLMDT", "EXCHNO", "EXCHID").build()
-		DBContainer EXT020 = query.getContainer();
-		EXT020.set("EXCONO", currentCompany);
-		EXT020.set("EXCUNO", cuno);
-		EXT020.set("EXASCD", ascd);
-		EXT020.setInt("EXFDAT", fdat as Integer);
-		if(!query.readAll(EXT020, 4, outData)){
-			mi.error("L'enregistrement existe déjà");
-			return;
-		}
-	}
+    if (mi.in.get("FDAT") != null) {
+      fdat = mi.in.get("FDAT")
+      if (!utility.call("DateUtil", "isDateValid", fdat, "yyyyMMdd")) {
+        mi.error("Format Date de Validité incorrect")
+        return
+      }
+    } else {
+      mi.error("Date de Validité est obligatoire")
+      return
+    }
+    LocalDateTime timeOfCreation = LocalDateTime.now()
+    DBAction ext032Query = database.table("EXT020").index("00").selection("EXCONO", "EXASCD", "EXCUNO", "EXFDAT", "EXSTAT", "EXSTTS", "EXNDTS", "EXRGDT", "EXRGTM", "EXLMDT", "EXCHNO", "EXCHID").build()
+    DBContainer ext032Request = ext032Query.getContainer()
+    ext032Request.set("EXCONO", currentCompany)
+    ext032Request.set("EXCUNO", cuno)
+    ext032Request.set("EXASCD", ascd)
+    ext032Request.setInt("EXFDAT", fdat as Integer)
+    if (!ext032Query.readAll(ext032Request, 4, 10000, ext020Reader)) {
+      mi.error("L'enregistrement n'existe pas")
+      return
+    }
+  }
 
-	Closure<?> outData = { DBContainer EXT020 ->
-		String cono = EXT020.get("EXCONO");
-		String cuno = EXT020.get("EXCUNO");
-		String ascd = EXT020.get("EXASCD");
-		String fdat = EXT020.get("EXFDAT");
-		String stat = EXT020.get("EXSTAT");
-		String stts = EXT020.get("EXSTTS");
-		String ndts = EXT020.get("EXNDTS");
-		String entryDate = EXT020.get("EXRGDT");
-		String entryTime = EXT020.get("EXRGTM");
-		String changeDate = EXT020.get("EXLMDT");
-		String changeNumber = EXT020.get("EXCHNO");
-		String changedBy = EXT020.get("EXCHID");
+  Closure<?> ext020Reader = { DBContainer ext020Resut ->
+    String cono = ext020Resut.get("EXCONO")
+    String cuno = ext020Resut.get("EXCUNO")
+    String ascd = ext020Resut.get("EXASCD")
+    String fdat = ext020Resut.get("EXFDAT")
+    String stat = ext020Resut.get("EXSTAT")
+    String stts = ext020Resut.get("EXSTTS")
+    String ndts = ext020Resut.get("EXNDTS")
+    String entryDate = ext020Resut.get("EXRGDT")
+    String entryTime = ext020Resut.get("EXRGTM")
+    String changeDate = ext020Resut.get("EXLMDT")
+    String changeNumber = ext020Resut.get("EXCHNO")
+    String changedBy = ext020Resut.get("EXCHID")
 
-		mi.outData.put("CONO", cono);
-		mi.outData.put("CUNO", cuno);
-		mi.outData.put("ASCD", ascd);
-		mi.outData.put("FDAT", fdat);
-		mi.outData.put("STAT", stat);
-		mi.outData.put("STTS", stts);
-		mi.outData.put("NDTS", ndts);
-		mi.outData.put("RGDT", entryDate);
-		mi.outData.put("RGTM", entryTime);
-		mi.outData.put("LMDT", changeDate);
-		mi.outData.put("CHNO", changeNumber);
-		mi.outData.put("CHID", changedBy);
-		mi.write();
-	}
+    mi.outData.put("CONO", cono)
+    mi.outData.put("CUNO", cuno)
+    mi.outData.put("ASCD", ascd)
+    mi.outData.put("FDAT", fdat)
+    mi.outData.put("STAT", stat)
+    mi.outData.put("STTS", stts)
+    mi.outData.put("NDTS", ndts)
+    mi.outData.put("RGDT", entryDate)
+    mi.outData.put("RGTM", entryTime)
+    mi.outData.put("LMDT", changeDate)
+    mi.outData.put("CHNO", changeNumber)
+    mi.outData.put("CHID", changedBy)
+    mi.write()
+  }
 }
--- src/main/groovy/LstAssortCriter.groovy
+++ src/main/groovy/LstAssortCriter.groovy
@@ -1,123 +1,96 @@
 /**
- * README
  * This extension is used by Mashup
- *
  * Name : EXT020MI.LstAssortCriter
+ * COMX01 Gestion des assortiments clients
  * Description : The LstAssortCriter transaction list records to the EXT020 table.
  * Date         Changed By   Description
  * 20220112     YBLUTEAU     COMX01 - Add assortment
+ * 20240620     FLEBARS       COMX01 - Controle code pour validation Infor
  */
- import java.time.LocalDateTime;
- import java.time.format.DateTimeFormatter;
- public class LstAssortCriter extends ExtendM3Transaction {
-   private final MIAPI mi;
-   private final DatabaseAPI database
-   private final LoggerAPI logger
-   private final MICallerAPI miCaller;
-   private final ProgramAPI program;
-   private final UtilityAPI utility;
- 
-   public LstAssortCriter(MIAPI mi, DatabaseAPI database, LoggerAPI logger, ProgramAPI program,UtilityAPI utility) {
-	 this.mi = mi;
-	 this.database = database;
-	 this.logger = logger;
-	 this.program = program;
-	 this.utility = utility;
-   }
- 
-   public void main() {
-	 Integer currentCompany;
-	 String cuno = "";
-	 String ascd = "";
-	 String fdat ="";
-	 if (mi.in.get("CONO") == null) {
-	   currentCompany = (Integer)program.getLDAZD().CONO;
-	 } else {
-	   currentCompany = mi.in.get("CONO");
-	 }
- 
-	 if(mi.in.get("CUNO") != null){
-	   DBAction countryQuery = database.table("OCUSMA").index("00").build();
-	   DBContainer OCUSMA = countryQuery.getContainer();
-	   OCUSMA.set("OKCONO",currentCompany);
-	   OCUSMA.set("OKCUNO",mi.in.get("CUNO"));
-	   if (!countryQuery.read(OCUSMA)) {
-		 mi.error("Code Client " + mi.in.get("CUNO") + " n'existe pas");
-		 return;
-	   }
-	   cuno = mi.in.get("CUNO");
-	 }
- 
-	 if(mi.in.get("ASCD") != null){
-	   DBAction countryQuery = database.table("CSYTAB").index("00").build();
-	   DBContainer CSYTAB = countryQuery.getContainer();
-	   CSYTAB.set("CTCONO",currentCompany);
-	   CSYTAB.set("CTSTCO",  "ASCD");
-	   CSYTAB.set("CTSTKY", mi.in.get("ASCD"));
-	   if (!countryQuery.read(CSYTAB)) {
-		 mi.error("Code Assortiment  " + mi.in.get("ASCD") + " n'existe pas");
-		 return;
-	   }
-	   ascd = mi.in.get("ASCD");
-	 }
- 
-	 if(mi.in.get("FDAT") != null){
-	   fdat = mi.in.get("FDAT");
-	   if (!utility.call("DateUtil", "isDateValid", fdat, "yyyyMMdd")) {
-		 mi.error("Format Date de Validité incorrect");
-		 return;
-	   }
-	 }
- 
-	 //Create Expression
-	 ExpressionFactory expression = database.getExpressionFactory("EXT020");
-	 expression = expression.eq("EXCONO", currentCompany.toString());
-	 if(cuno!=""){
-	   expression =  expression.and(expression.ge("EXCUNO", cuno));
-	 }
-	 if(ascd!="") {
-	   expression = expression.and(expression.ge("EXASCD", ascd));
-	 }
-	 if(fdat!="") {
-	   expression = expression.and(expression.ge("EXFDAT", fdat));
-	 }
-	 //Run Select
-	 DBAction query = database.table("EXT020").index("00").matching(expression).selection("EXCONO", "EXASCD", "EXCUNO", "EXFDAT", "EXSTAT", "EXSTTS", "EXNDTS", "EXRGDT", "EXRGTM", "EXLMDT", "EXCHNO", "EXCHID").build();
-	 DBContainer EXT020 = query.getContainer();
-	 EXT020.setInt("EXCONO",currentCompany);
-	 if(!query.readAll(EXT020, 1, outData)){
-	   mi.error("L'enregistrement n'existe pas");
-	   return;
-	 }
-   }
-   Closure<?> outData = { DBContainer EXT020 ->
-	 String cono = EXT020.get("EXCONO");
-	 String ascd = EXT020.get("EXASCD");
-	 String cuno = EXT020.get("EXCUNO");
-	 String fdat = EXT020.get("EXFDAT");
-	 String stat = EXT020.get("EXSTAT");
-	 String stts = EXT020.get("EXSTTS");
-	 String ndts = EXT020.get("EXNDTS");
- 
-	 String entryDate = EXT020.get("EXRGDT");
-	 String entryTime = EXT020.get("EXRGTM");
-	 String changeDate = EXT020.get("EXLMDT");
-	 String changeNumber = EXT020.get("EXCHNO");
-	 String changedBy = EXT020.get("EXCHID");
- 
-	 mi.outData.put("CONO", cono);
-	 mi.outData.put("CUNO", cuno);
-	 mi.outData.put("ASCD", ascd);
-	 mi.outData.put("FDAT", fdat);
-	 mi.outData.put("STAT", stat);
-	 mi.outData.put("STTS", stts);
-	 mi.outData.put("NDTS", ndts);
-	 mi.outData.put("RGDT", entryDate);
-	 mi.outData.put("RGTM", entryTime);
-	 mi.outData.put("LMDT", changeDate);
-	 mi.outData.put("CHNO", changeNumber);
-	 mi.outData.put("CHID", changedBy);
-	 mi.write();
-   }
- }
- 
+public class LstAssortCriter extends ExtendM3Transaction {
+  private final MIAPI mi;
+  private final DatabaseAPI database
+  private final LoggerAPI logger
+  private final ProgramAPI program;
+  private final UtilityAPI utility;
+
+  private int currentCompany
+
+  public LstAssortCriter(MIAPI mi, DatabaseAPI database, LoggerAPI logger, ProgramAPI program,UtilityAPI utility) {
+    this.mi = mi
+    this.database = database
+    this.logger = logger
+    this.program = program
+    this.utility = utility
+  }
+
+  public void main() {
+    String cuno = ""
+    String ascd = ""
+    String fdat =""
+    if (mi.in.get("CONO") == null) {
+      currentCompany = (Integer)program.getLDAZD().CONO
+    } else {
+      currentCompany = mi.in.get("CONO") as Integer
+    }
+
+    if(mi.in.get("FDAT") != null){
+      fdat = mi.in.get("FDAT")
+      if (!utility.call("DateUtil", "isDateValid", fdat, "yyyyMMdd")) {
+        mi.error("Format Date de Validité incorrect")
+        return
+      }
+    }
+
+    //Create Expression
+    ExpressionFactory ext020Expression = database.getExpressionFactory("EXT020")
+    ext020Expression = ext020Expression.eq("EXCONO", currentCompany.toString())
+    if(cuno!=""){
+      ext020Expression =  ext020Expression.and(ext020Expression.ge("EXCUNO", cuno))
+    }
+    if(ascd!="") {
+      ext020Expression = ext020Expression.and(ext020Expression.ge("EXASCD", ascd))
+    }
+    if(fdat!="") {
+      ext020Expression = ext020Expression.and(ext020Expression.ge("EXFDAT", fdat))
+    }
+    //Run Select
+    DBAction ext020Query = database.table("EXT020").index("00").matching(ext020Expression).selection("EXCONO", "EXASCD", "EXCUNO", "EXFDAT", "EXSTAT", "EXSTTS", "EXNDTS", "EXRGDT", "EXRGTM", "EXLMDT", "EXCHNO", "EXCHID").build()
+    DBContainer ext020Request = ext020Query.getContainer()
+    ext020Request.setInt("EXCONO",currentCompany)
+    if(!ext020Query.readAll(ext020Request, 1, ext020Reader)){
+      mi.error("L'enregistrement n'existe pas")
+      return
+    }
+  }
+  Closure<?> ext020Reader = { DBContainer ext020Result ->
+    String cono = ext020Result.get("EXCONO")
+    String ascd = ext020Result.get("EXASCD")
+    String cuno = ext020Result.get("EXCUNO")
+    String fdat = ext020Result.get("EXFDAT")
+    String stat = ext020Result.get("EXSTAT")
+    String stts = ext020Result.get("EXSTTS")
+    String ndts = ext020Result.get("EXNDTS")
+
+    String entryDate = ext020Result.get("EXRGDT")
+    String entryTime = ext020Result.get("EXRGTM")
+    String changeDate = ext020Result.get("EXLMDT")
+    String changeNumber = ext020Result.get("EXCHNO")
+    String changedBy = ext020Result.get("EXCHID")
+
+    mi.outData.put("CONO", cono)
+    mi.outData.put("CUNO", cuno)
+    mi.outData.put("ASCD", ascd)
+    mi.outData.put("FDAT", fdat)
+    mi.outData.put("STAT", stat)
+    mi.outData.put("STTS", stts)
+    mi.outData.put("NDTS", ndts)
+    mi.outData.put("RGDT", entryDate)
+    mi.outData.put("RGTM", entryTime)
+    mi.outData.put("LMDT", changeDate)
+    mi.outData.put("CHNO", changeNumber)
+    mi.outData.put("CHID", changedBy)
+    mi.write()
+  }
+}
+
